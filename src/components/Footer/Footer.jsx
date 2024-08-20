c198d58b8a0b8d65b8d52087d1817ad106474a37
Ankit <mr.pandeyankit14@gmail.com>
2024-08-20 23:03:28 +0530

    Deployed

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div>
+  <p className='footer'>Copyright @2024, Cyptoplace - All Right Reserved</p>
+
+    </div>
+  )
+}
+
+
+export default Footer
